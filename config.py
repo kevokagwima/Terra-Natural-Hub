ff2cf8e8fac64afc2dca544ee3c8b01f587f81f8
kevokagwima <kevokagwima@gmail.com>
2025-08-05 09:37:38 +0300

    updated files

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 
 class Config:
-  SQLALCHEMY_DATABASE_URI = "mysql+mysqldb://root:Hunter9039@localhost/terra"
+  # SQLALCHEMY_DATABASE_URI = "mysql+mysqldb://root:Hunter9039@localhost/terra"
+  SQLALCHEMY_DATABASE_URI = "mysql+mysqldb://junglefowl:wV9-kZ1=hK1=dF5=oI6=@localhost/minimum-indigo-orangutan"
   SQLALCHEMY_TRACK_MODIFICATIONS = False
   SESSION_PERMANENT = False
   SESSION_TYPE = "filesystem"
